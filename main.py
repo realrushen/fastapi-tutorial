e5a282fbdb73b2c31a3d39d7be5d79c1ae8eea57
Karakchiev Aleksandr <realrushen@gmail.com>
2022-08-27 22:47:14 +0300

    Query Parameters

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get("/")
 async def root():
@@ -23,8 +25,35 @@ async def say_hello(name: str):
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/items/")
+async def read_item2(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
 
 
 @app.get("/users/me")
