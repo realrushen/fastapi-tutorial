f9bf2ada7b5b3cc1d0cc709d9d6feb05a30497b0
Karakchiev Aleksandr <realrushen@gmail.com>
2022-08-28 09:40:08 +0300

    Extra Data Types

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from datetime import datetime, time, timedelta
+from uuid import UUID
+
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "name": "Foo",
-                "description": "A very nice Item",
-                "price": 35.4,
-                "tax": 3.2,
-            }
-        }
-
-
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
+async def read_items(
+        item_id: UUID,
+        start_datetime: datetime | None = Body(default=None),
+        end_datetime: datetime | None = Body(default=None),
+        repeat_at: time | None = Body(default=None),
+        process_after: timedelta | None = Body(default=None),
+):
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration,
+    }
