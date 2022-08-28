7cd338ad2e7a7d172245120ea3135c8442ad5525
Karakchiev Aleksandr <realrushen@gmail.com>
2022-08-28 09:55:01 +0300

    Response Model

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-from datetime import datetime, time, timedelta
-from uuid import UUID
-
-from fastapi import Body, FastAPI
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.put("/items/{item_id}")
-async def read_items(
-        item_id: UUID,
-        start_datetime: datetime | None = Body(default=None),
-        end_datetime: datetime | None = Body(default=None),
-        repeat_at: time | None = Body(default=None),
-        process_after: timedelta | None = Body(default=None),
-):
-    start_process = start_datetime + process_after
-    duration = end_datetime - start_process
-    return {
-        "item_id": item_id,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration,
-    }
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float = 10.5
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
+    "baz": {
+        "name": "Baz",
+        "description": "There goes my baz",
+        "price": 50.2,
+        "tax": 10.5,
+    },
+}
+
+
+@app.get(
+    "/items/{item_id}/name",
+    response_model=Item,
+    response_model_include=["name", "description"],
+)
+async def read_item_name(item_id: str):
+    return items[item_id]
+
+
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude=["tax"])
+async def read_item_public_data(item_id: str):
+    return items[item_id]
